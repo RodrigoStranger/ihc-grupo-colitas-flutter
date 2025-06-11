c438c687764ff31928c5a0a26c0c83cfb63fe3b5
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-06-11 13:17:20 -0500

    update

--- lib/core/colors.dart
+++ lib/core/colors.dart
@@ -4,3 +4,4 @@ const Color primaryPastelBlue = Color(0xFFB8E6F0);
 const Color darkPastelBlue = Color(0xFF87CEEB);
 const Color lightPastelBlue = Color(0xFFE1F5FE);
 const Color accentBlue = Color(0xFF4FC3F7);
+const Color labelTextColor = Colors.black;
